4102bc2a8d783f5417e8f5590f92963bb4d74a98
Gary White <glw494@gmail.com>
2021-10-22 07:43:29 -0400

    UI rough draft

--- app.js
+++ app.js
@@ -1,3 +1,58 @@
+// UI
+
+let firstNumber = 0;
+let secondNumber = 0;
+let operandActive = false;
+let currentOperand = "";
+let previousOperand = "";
+
+const display = document.getElementById("calc-display");
+const deleteButton = document.querySelector(".delete");
+const decimalButton = document.querySelector(".decimal");
+
+const clearButton = document.querySelector(".clear");
+clearButton.addEventListener("click", () => clearAll());
+
+function clearAll() {
+    display.textContent = "";
+    firstNumber = 0;
+    secondNumber = 0;
+    currentOperand = "";
+    operandActive = false;
+}
+
+const equalsButton = document.querySelector(".equals");
+equalsButton.addEventListener("click", () => {
+    const answer = operate(currentOperand, parseInt(firstNumber), parseInt(secondNumber));
+    firstNumber = answer;
+    display.textContent = answer;
+    secondNumber = 0;
+    operandActive = false;
+})
+
+let numberButtons = document.querySelectorAll(".number");
+numberButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        if (operandActive == true) {
+            secondNumber += button.dataset.value;
+        }
+        if (operandActive == false) {
+            firstNumber += button.dataset.value;
+        }
+
+        display.textContent += button.dataset.value;
+    })
+})
+
+let operandButtons = document.querySelectorAll(".operand");
+operandButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        operandActive = true;
+        currentOperand = button.dataset.value;
+        display.textContent = "";
+    })
+})
+
 // Math functions
 
 function add(a,b) {
@@ -18,13 +73,13 @@ function divide(a,b) {
 
 function operate(operand, a, b) {
     switch (operand) {
-        case add:
+        case "add":
             return add(a,b)
-        case subtract:
+        case "subtract":
             return subtract(a,b)
-        case multiply:
+        case "multiply":
             return multiply(a,b)
-        case divide:
+        case "divide":
             return divide(a,b)
     }
 }
